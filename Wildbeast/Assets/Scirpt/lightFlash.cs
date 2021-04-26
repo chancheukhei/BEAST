1370d38ef03978b540fe79f09b152b609e60ece8
FungChakYu <200069393@stu.vtc.edu.hk>
2021-04-26 15:29:53 +0800

    light

---
+++ Wildbeast/Assets/Scirpt/lightFlash.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightFlash : MonoBehaviour
+{
+
+    public bool lightOn = true;       // light swich
+    public float turnspeed = 5;  // light flashing speed
+    private float highIntensity = 20f;
+    private float lowIntensity = 5;
+    private float targetIntensity = 5;
+    private Light flashLight;
+
+    void Start()
+    {
+        // get light component
+        flashLight = GameObject.FindWithTag("flashLight").GetComponent<Light>();
+        
+        // set target light intensity to high
+        targetIntensity = highIntensity;
+    }
+
+    void Update()
+    {
+        // if turn on the light, light flashing
+        if (lightOn)
+        {
+            // swich intensity to high
+            flashLight.intensity = Mathf.Lerp(flashLight.intensity, targetIntensity, Time.deltaTime * turnspeed);
+            if (Mathf.Abs(targetIntensity - flashLight.intensity) <= 0.05f)
+            {
+                targetIntensity = targetIntensity == highIntensity ? lowIntensity : highIntensity;
+            }
+        }
+        else
+        {
+            // swich light to low intensity
+            flashLight.intensity = Mathf.Lerp(flashLight.intensity, lowIntensity, Time.deltaTime * turnspeed);
+
+            if (flashLight.intensity < 0.1f)
+            {
+                flashLight.intensity = lowIntensity;
+            }
+        }
+            
+    }
+}
