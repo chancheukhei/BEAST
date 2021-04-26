ce8806c9f3d10963aafd1a4419018ba286e58bab
FungChakYu <200069393@stu.vtc.edu.hk>
2021-04-26 16:10:25 +0800

    地雷setting

---
+++ Wildbeast/Assets/Scirpt/BonceOffMe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonceOffMe : MonoBehaviour
+{
+    AudioSource bonceSound;
+    void Start()
+    {
+        bonceSound = GameObject.FindWithTag("boncer").GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.transform.position = col.gameObject.transform.position*2;
+            bonceSound.Play();
+        }
+    }
+}
