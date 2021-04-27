d3c9c4341a09c57229342bf7239d39470c74421a
FungChakYu <200069393@stu.vtc.edu.hk>
2021-04-27 11:29:49 +0800

    AI PathFinding

---
+++ Wildbeast/Assets/Scirpt/Portal3.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Portal3 : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision Detected");
+            SceneManager.LoadScene("WinGame");
+        }
+    }
+
+
+}
